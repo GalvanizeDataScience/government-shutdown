3342c68ea6b0d5e79bf858b1640924bdaacbd5e5
Thomas Levine <occurrence@thomaslevine.com>
2013-10-09 11:54:25 -0700

    boilerplate

--- boilerplate.r
+++ boilerplate.r
@@ -6,9 +6,33 @@ if (!(file.exists('treasury_data.db'))) {
 }
 
 # Load dependencies
-for (.library in c('ggplot2','sqldf')) {
-  if (!(require(.library))) {
-    install.packages(.library)
-    library(.library)
-  }
-}
+library(ggplot2)
+library(sqldf)
+
+# Run this if the "library" commands fail.
+# install.packages(c('ggplot2','sqldf'))
+
+
+
+
+# Querying
+t3a_head <- sqldf('SELECT * FROM t3a LIMIT 100;', dbname = 'treasury_data.db')
+
+# Note: sqldf queries both from the SQLite3 database
+# and from the current R session.
+
+print(sqldf('SELECt sum(today) FROM t3a_head'), dbname = 'treasury_data.db')
+
+# Note well: DO NOT CREATE VARIABLES WITH THE NAMES OF ANY OF THE TABLES.
+# If you do that, sqldf will query the table from the R session rather than
+# from the SQLite3 database.
+
+
+
+
+# Plotting
+#
+# There are a lot of syntaxes you can use with ggplot.
+# This one is probably the most verbose format you can get,
+# but it makes the different components quite clear.
+# ggplot(
