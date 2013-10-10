3335c29b1bd96a495474f7b1d36dce07f65ca87f
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:43:38 -0700

    labels

--- year.start.r
+++ year.start.r
@@ -1,16 +1,20 @@
 library(sqldf)
 library(ggplot2)
+library(scales)
 
 start.v.mean <- sqldf('select * from start_versus_mean', dbname = 'treasury_data.db')
+"
 molten <- melt(start.v.mean,
                id.vars = c('item','transaction_type','year','date'),
                measure.vars = c('today','mean'),
                variable.name = 'statistic', value.name = 'amount')
+"
 
-start.v.mean$difference <- start.v.mean$today - start.v.mean$mean
+start.v.mean$difference <- start.v.mean$today / start.v.mean$mean
 start.v.mean$item <- factor(start.v.mean$item,
   levels = unique(start.v.mean$item[order(start.v.mean$difference)]))
 
 p <- ggplot(start.v.mean) +
   aes(x = item, group = transaction_type, color = transaction_type, y = difference) +
-  geom_line() + coord_flip()
+  geom_line() + coord_flip() +
+  scale_y_log10('Amount for year start over mean amount for the year', labels = dollar)
