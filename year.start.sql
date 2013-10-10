d90e9c6b796990eef33f4dfceda92ed92caceca5
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:25:04 -0700

    year mean

--- year.start.sql
+++ year.start.sql
@@ -1,11 +1,29 @@
 DROP VIEW IF EXISTS "year_start";
 CREATE VIEW "year_start" AS SELECT
-  "date",
   "item",
-  "today",
-  "year"
+  "transaction_type",
+  "year",
+  "date",
+  "today"
 FROM (SELECT * FROM "t2" ORDER BY "date" DESC)
 WHERE
   "month" = 10 AND "day" <= 3 AND
   ("transaction_type" = 'deposit' OR "transaction_type" = 'withdrawal')
 GROUP BY "item", "transaction_type", "year";
+
+DROP VIEW IF EXISTS "year_mean";
+CREATE VIEW "year_mean" AS SELECT
+  "item",
+  "transaction_type",
+  "year",
+  sum("today")/count("today") AS 'mean'
+FROM "t2"
+WHERE "transaction_type" = 'deposit' OR "transaction_type" = 'withdrawal'
+GROUP BY "item", "transaction_type", "year";
+
+SELECT year_start.item, year_start.transaction_type, year_start.year, year_start."date", year_start.today, year_mean.mean
+FROM year_start JOIN year_mean
+ON year_start.item = year_mean.item AND
+   year_start.transaction_type = year_mean.transaction_type AND
+   year_start.year = year_mean.year
+LIMIT 10;
