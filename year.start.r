c2008faa5c737db26f922d7141025e1b6d65b6b9
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:40:56 -0700

    ordering the levels

--- year.start.r
+++ year.start.r
@@ -1,7 +1,16 @@
 library(sqldf)
+library(ggplot2)
 
 start.v.mean <- sqldf('select * from start_versus_mean', dbname = 'treasury_data.db')
 molten <- melt(start.v.mean,
                id.vars = c('item','transaction_type','year','date'),
                measure.vars = c('today','mean'),
                variable.name = 'statistic', value.name = 'amount')
+
+start.v.mean$difference <- start.v.mean$today - start.v.mean$mean
+start.v.mean$item <- factor(start.v.mean$item,
+  levels = unique(start.v.mean$item[order(start.v.mean$difference)]))
+
+p <- ggplot(start.v.mean) +
+  aes(x = item, group = transaction_type, color = transaction_type, y = difference) +
+  geom_line() + coord_flip()
