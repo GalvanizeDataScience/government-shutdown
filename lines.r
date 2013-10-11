934214356b539602c77934ad541883d1a6f17d26
Thomas Levine <occurrence@thomaslevine.com>
2013-10-11 11:41:16 -0700

    aoeu

--- lines.r
+++ lines.r
@@ -4,9 +4,11 @@ library(scales)
 library(plyr)
 
 if (!('d.full' %in% ls())) {
-  d.full <- sqldf('select date, today, weekday, item, transaction_type from t2', dbname = 'treasury_data.db')
+  d.full <- sqldf('select date, today, weekday, day, item, transaction_type from t2', dbname = 'treasury_data.db')
   d.full$date <- as.Date(d.full$date)
   d.full$weekday <- factor(d.full$weekday, levels = c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'))
+  d.full$first.three.days <- factor(d.full$day <= 3, levels = c(TRUE, FALSE))
+  levels(d.full$first.three.days) <- c('Yes', 'No')
 }
 
 d.subset <- subset(d.full,
@@ -15,9 +17,20 @@ d.subset <- subset(d.full,
   item == 'Medicare' |
   item == 'Medicaid' )
 d.subset <- subset(d.subset, transaction_type == 'withdrawal')
+d.subset$item <- factor(d.subset$item)
 
-p <- ggplot(d.subset) +
+p1 <- ggplot(d.subset) +
   aes(x = date, y = today, group = item) +
   aes(shape = weekday, color = item) +
   scale_y_log10('Amount') + scale_x_date('Date') +
   geom_line() + geom_point()
+
+p2 <- ggplot(d.subset) +
+  aes(x = date, y = today, group = item) +
+  aes(shape = first.three.days, color = item) +
+  scale_y_log10('Amount') + scale_x_date('Date') +
+  scale_shape_discrete('Is it the first, second or third of the month?') +
+  geom_line() + geom_point()
+
+#m <- lm(today ~ day + item, data = d.subset)
+plot(today ~ day + item, data = d.subset)
