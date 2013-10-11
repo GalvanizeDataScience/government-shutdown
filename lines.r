32b579513cc92d6bc1711ed9ffa8ccbc20bda791
Thomas Levine <occurrence@thomaslevine.com>
2013-10-11 11:27:37 -0700

    order the levels

--- lines.r
+++ lines.r
@@ -6,6 +6,7 @@ library(plyr)
 if (!('d.full' %in% ls())) {
   d.full <- sqldf('select date, today, weekday, item, transaction_type from t2', dbname = 'treasury_data.db')
   d.full$date <- as.Date(d.full$date)
+  d.full$weekday <- factor(d.full$weekday, levels = c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'))
 }
 
 d.subset <- subset(d.full,
@@ -18,5 +19,5 @@ d.subset <- subset(d.subset, transaction_type == 'withdrawal')
 p <- ggplot(d.subset) +
   aes(x = date, y = today, group = item) +
   aes(shape = weekday, color = item) +
-  geom_line() +
-  scale_y_log10('Amount')
+  scale_y_log10('Amount') + scale_x_date('Date') +
+  geom_line() + geom_point()
