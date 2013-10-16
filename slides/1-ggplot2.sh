bbd24c27de29457d35cf7576475acee665923d91
Thomas Levine <occurrence@thomaslevine.com>
2013-10-16 11:20:21 -0700

    more slides

--- slides/1-ggplot2.sh
+++ slides/1-ggplot2.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-wget -O 1-ggplot2.pdf http://ggplot2.org/resources/2007-vanderbilt.pdf
+set -e
+
+test -e 1-ggplot2-a.pdf || wget -O 1-ggplot2-a.pdf http://ggplot2.org/resources/2007-vanderbilt.pdf
+test -e 1-ggplot2-b.pdf || wget -O 1-ggplot2-b.pdf http://ggplot2.org/resources/2007-past-present-future.pdf
