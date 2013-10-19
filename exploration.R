0cf4361f5c4edc49cd0f1bd97b2382b46e0884af
paul <minton.paul@gmail.com>
2013-10-18 17:02:53 -0700

    EXPLORE

--- exploration.R
+++ exploration.R
@@ -1,20 +1,25 @@
 setwd("~/Zipfian/week5/government-shutdown")
 source('boilerplate.r')
+rm('t3a', 't3a_head', 't3b', 't3c', 't4', 't5', 't6')
 library(scales)
 library(plyr)
+library(reshape2)
+
 t1$date <- as.Date(t1$date)
 t1_subset <- subset(t1, account == 'Federal Reserve Account')
 t1_subset <- subset(t1_subset, date )
 # t1_subset[,'color'] <- NA
 t1_subset$recent <- sapply(t1_subset$date, function(x){if (x > as.Date('2013-08-01')) 1 else 0})
 
-initialplot <- ggplot(t1_subset) + aes(x = date, y = close_today, group = recent, color = factor(recent)) + geom_line()
+initialplot <- ggplot(t1_subset) + 
+  aes(x = date, y = close_today, group = recent, color = factor(recent)) + geom_line()
 print(initialplot)
 str(t1_subset)
 
 t2$date <- as.Date(t2$date)
 withdrawals <-t2[(t2$transaction_type == 'withdrawal') & (t2$is_net == 0) & (t2$date > as.Date('2008-01-01')),]
-top = names(sort(table(withdrawals$item), decreasing = TRUE)[1:100])
+withdrawals$recent <- sapply(withdrawals$date, function(x){if (x > as.Date('2013-08-15')) 1 else 0})
+top = names(sort(table(withdrawals$item), decreasing = TRUE)[1:50])
 withdrawals$greatest = factor(withdrawals$item, levels = top)
 top_item_wd <- withdrawals[withdrawals[,'item']%in%top,]
 #with_great_freq <- withdrawals[withdrawals$item%in%withdrawals$greatest]
@@ -22,7 +27,7 @@ p1 <- ggplot(top_item_wd)
 p1 <- p1 + aes(x = date, y = today, group = greatest, color = greatest) + geom_point(na.rm = TRUE)
 p1 + scale_y_log10(labels = dollar) 
 
-top_item_wd$recent <- sapply(top_item_wd$date, function(x){if (x > as.Date('2013-08-15')) 1 else 0})
+top_item_wd$recent <- sapply(top_item_wd$date, function(x){if (x > as.Date('2013-07-15')) 1 else 0})
 recent_mask = top_item_wd[,'recent'] == 1
 old_mask = top_item_wd[,'recent'] == 0
 recent_agg <- aggregate(today~item, data = top_item_wd[recent_mask,], FUN = mean)
@@ -34,7 +39,8 @@ added_agg = data.frame(Expense = c(recent_agg$item, old_agg$item),
 
 
 p2 <- ggplot(added_agg)
-p2 <- p2 + aes(x = Expense, y = Amount, group = factor(recent), fill = factor(recent))  + geom_bar(position = 'dodge',stat = 'identity') +
+p2 <- p2 + aes(x = Expense, y = Amount, group = factor(recent), fill = factor(recent))  + 
+  geom_bar(position = 'dodge',stat = 'identity') +
   scale_y_log10(labels = dollar) + labs(title = "Average Goverment Program Withdrawals") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
@@ -42,22 +48,34 @@ print(p2)
 
 merged_agg = merge(x = recent_agg, y = old_agg, by.x = 'item', by.y = 'item', all.y = TRUE)
 names(merged_agg) = c('item', 'recent', 'history')
-merged_agg[is.na(merged_agg)]<- .0000000001
-merged_agg[,]
+merged_agg[is.na(merged_agg)]<- 0
 merged_agg$diff = merged_agg$history - merged_agg$recent
 ggplot(merged_agg) + aes(x = item, y = diff) + geom_bar(stat = 'identity') +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_log10()
 
-p3 <- ggplot(merged_agg) + aes(x = item, y = recent, fill = 'red') + aes(x = item, y = history, fill = 'green') + 
-  geom_bar(stat = 'identity', position = 'dodge') + scale_y_log10() +
+unzero_merged <- merged_agg[(merged_agg$recent > 0) & (merged_agg$history > 0),]
+melted <- melt(data = unzero_merged, id = 'item')
+levels = melted$item[melted$variable == 'diff'][order(melted$value[melted$variable == 'diff'])]
+melted$item <- factor(melted$item, levels = levels)
+p3 <- ggplot(melted[melted$variable != 'diff',],) +
+  aes(x=item, y =value, group = variable, color = variable) +
+  geom_line(stat = 'identity', position = 'dodge') + 
+  scale_y_log10(labels = dollar) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(p3)
+
+melted$sort <- match(melted$value[melted$variable == 'diff'], sort(melted$value[melted$variable == 'diff']))
+
 
 pos_names = merged_agg[merged_agg$diff > 0,'item']
 neg_names = merged_agg[merged_agg$diff <= 0,'item']
 pos_subset = top_item_wd[top_item_wd[,'item']%in%pos_names,]
 neg_subset = top_item_wd[top_item_wd[,'item']%in%neg_names,]
-ggplot(neg_subset[neg_subset[,'date']>as.Date('2013-9-01'),]) + aes(x = date, y = today, group = item, color = item) + geom_line() + scale_y_continuous(labels = dollar)
+ggplot(neg_subset[neg_subset[,'date']>as.Date('2013-9-01'),]) + 
+  aes(x = date, y = today, group = item, color = item) + geom_bar() +
+  scale_y_continuous(labels = dollar)
+
+ggplot(withdrawals)
 
 #ggsave(filename='shutdown_bar.jpg', plot = p2)
 #ggsave(filename='shutdown_point.jpg', plot = p1)
