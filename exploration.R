1f59c6b03c75b6675c5be3afa860a5f63118507e
paul <minton.paul@gmail.com>
2013-10-20 00:48:23 -0700

    almost have something pretty

--- exploration.R
+++ exploration.R
@@ -20,7 +20,7 @@ str(t1_subset)
 t2$date <- as.Date(t2$date)
 withdrawals <-t2[(t2$transaction_type == 'withdrawal') & (t2$is_net == 0) & (t2$date > as.Date('2008-01-01')),]
 #withdrawals$recent <- sapply(withdrawals$date, function(x){if (x > as.Date('2013-08-15')) 1 else 0})
-top = names(sort(table(withdrawals$item), decreasing = TRUE)[1:50])
+top = names(sort(table(withdrawals$item), decreasing = TRUE)[1:100])
 withdrawals$greatest = factor(withdrawals$item, levels = top)
 top_item_wd <- withdrawals[withdrawals[,'item']%in%top,]
 #with_great_freq <- withdrawals[withdrawals$item%in%withdrawals$greatest]
@@ -31,33 +31,41 @@ p1 + scale_y_log10(labels = dollar)
 unzero_merge_sort1 <- split_merge_diff(top_item_wd = top_item_wd, date1 = as.Date('2012-08-15'),
                                        date2 = as.Date('2012-09-15'),
                                        date3 = as.Date('2011-08-15'),
-                                       date4 = as.Date('2011-09-15'))
+                                       date4 = as.Date('2011-09-15'))[[1]]
 unzero_merge_sort2 <- split_merge_diff(top_item_wd = top_item_wd, date1 = as.Date('2013-08-15'),
                                        date2 = as.Date('2013-09-15'),
                                        date3 = as.Date('2012-08-15'),
-                                       date4 = as.Date('2012-09-15'))
+                                       date4 = as.Date('2012-09-15'))[[1]]
 unzero_merge_sort3 <- split_merge_diff(top_item_wd = top_item_wd, date1 = as.Date('2011-08-15'),
                                        date2 = as.Date('2011-09-15'),
                                        date3 = as.Date('2010-08-15'),
-                                       date4 = as.Date('2010-09-15'))
-
+                                       date4 = as.Date('2010-09-15'))[[1]]
 
 get_rid <- c("Public Debt Cash Redemp ( Table III B )","Total Federal Reserve Account" ,
              "Total Withdrawals ( excluding transfers )" )
-p3 <- ggplot(unzero_merge_sort1[!(unzero_merge_sort1$item %in% get_rid),]) +
+
+unzero_merge_sort1[(!(unzero_merge_sort1$item %in% get_rid)&(unzero_merge_sort1$abs == 1))]
+
+p5 <- ggplot(unzero_merge_sort3[!(unzero_merge_sort3$item %in% get_rid),]) +
   aes(x=factor(item, levels = unique(item)), y =diff, group = abs, fill = factor(abs)) +
+  geom_bar(stat = 'identity', position = 'dodge') + 
+  scale_y_log10(breaks = c(10,100,1000,10000),labels = dollar) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+print(p5)
+
+p3 <- ggplot(unzero_merge_sort1[(!(unzero_merge_sort1$item %in% get_rid)&(unzero_merge_sort1$abs == 1)),]) +
+  aes(x=factor(item, levels = unique(item)), y =diff) + #group = abs, fill = factor(abs)) +
   geom_bar(stat = 'identity', position = 'dodge') + 
   scale_y_log10(breaks = c(10,100,1000,10000),labels = dollar) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(p3)
 
-p4 <- ggplot(unzero_merge_sort1[!(unzero_merge_sort2$item %in% get_rid),]) +
-  aes(x=factor(item, levels = unique(item)), y =diff, group = abs, fill = factor(abs)) +
+p4 <- ggplot(unzero_merge_sort2[(!(unzero_merge_sort2$item %in% get_rid)&(unzero_merge_sort2$abs == 1)),]) +
+  aes(x=factor(item, levels = unique(item)), y =diff) + # group = abs, fill = factor(abs)) +
   geom_bar(stat = 'identity', position = 'dodge') + 
   scale_y_log10(breaks = c(10,100,1000,10000),labels = dollar) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(p4)
-
 
 
 pos_names = merged_agg[merged_agg$diff > 0,'item']
--- functions.r
+++ functions.r
@@ -10,27 +10,36 @@ label_subset <- function(x, newdate1, newdate2, olddate1, olddate2){
 }
 
 split_merge_diff <- function(top_item_wd, date1, date2, date3, date4){
+  # assign to each row it's "recentness"
   top_item_wd$recent <- sapply(top_item_wd$date, label_subset, newdate1 = date1, newdate2 = date2, 
                                olddate1 = date3, olddate2 = date4)
+  # create a mask to subset by recentness
+  # also count the frequency of item withdrawals
   recent_mask = top_item_wd$recent == 1
   recent_mask[is.na(recent_mask)] <- FALSE
   old_mask = top_item_wd$recent == 0
   old_mask[is.na(old_mask)] <- FALSE
+  recent_freq <- count(top_item_wd[recent_mask,'item'])
+  old_freq <- count(top_item_wd[old_mask,'item'])
+  # collapse the tables by the items, taking the average of all the withdawals
   recent_agg <- aggregate(today~item, data = top_item_wd[recent_mask,], FUN = mean)
   old_agg <- aggregate(today~item, data = top_item_wd[old_mask,], FUN = mean)
+  # join the collapsed tables with their frequencies
+  recent_agg_freq <- merge(recent_agg, recent_freq, by.x = 'item', by.y = 'x')
+  old_agg_freq <- merge(old_agg, old_freq, by.x = 'item', by.y = 'x')
+  merged_agg = merge(x = recent_agg_freq, y = old_agg_freq, by.x = 'item', by.y = 'item', all.y = TRUE)
   
-  merged_agg = merge(x = recent_agg, y = old_agg, by.x = 'item', by.y = 'item', all.y = TRUE)
-  names(merged_agg) = c('item', 'recent', 'history')
+  names(merged_agg) = c('item', 'recent', 'recent_freq', 'history', 'hostorical_freq')
   merged_agg[is.na(merged_agg)]<- 0
   merged_agg$diff = merged_agg$history - merged_agg$recent
-  ggplot(merged_agg) + aes(x = item, y = diff) + geom_bar(stat = 'identity') +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_log10()
-  
-  unzero_merged <- merged_agg[(merged_agg$recent > 0) & (merged_agg$history > 0),]
-  
-  
-  unzero_merge_sort <- unzero_merged[order(unzero_merged$diff),]
-  unzero_merge_sort$abs <- sapply(unzero_merge_sort$diff, function(x) {if (x <= 0) 1 else 0})
-  unzero_merge_sort$diff <- sapply(unzero_merge_sort$diff, function(x) {if (x <= 0) abs(x) else x})
-  return(list(unzero_merge_sort,old_mask, recent_mask))
-}
+   #ggplot(merged_agg) + aes(x = item, y = diff) + geom_bar(stat = 'identity') +
+   #  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_log10()
+   
+   unzero_merged <- merged_agg#[(merged_agg$recent > 0) & (merged_agg$history > 0),]
+   
+   
+   unzero_merge_sort <- unzero_merged[order(unzero_merged$diff),]
+   unzero_merge_sort$abs <- sapply(unzero_merge_sort$diff, function(x) {if (x <= 0) 1 else 0})
+   unzero_merge_sort$diff <- sapply(unzero_merge_sort$diff, function(x) {if (x <= 0) abs(x) + 1 else x})
+   return(list(unzero_merge_sort,old_mask, recent_mask))
+ }
